6190248c8c6b87ecfe6019878497fad71e150645
piyush01-01 <piyush24888@gmail.com>
2025-08-25 01:15:32 +0530

    latest

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -320,7 +320,7 @@ const SignupPage = () => {
                   <button
                     type="button"
                     className="w-full btn-outline flex justify-center items-center"
-                    onClick={() => toast.info('Google OAuth integration coming soon!')}
+                    onClick={() => toast('Google OAuth integration coming soon!')}
                   >
                     <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24">
                       <path
